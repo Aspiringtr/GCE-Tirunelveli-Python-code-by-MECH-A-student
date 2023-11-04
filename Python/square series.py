d8885423b4509c431c69bfabf94aae0d74c84259
Aspiringtr <127831054+Aspiringtr@users.noreply.github.com>
2023-11-04 20:40:17 +0530

    Delete Python directory

--- Python/square series.py
+++
@@ -1,3 +0,0 @@
-n=int(input("Enter the n number of square series:"))
-for i in range(1,n+1):
-    print(i**2)
--- Python/star pattern.py
+++
@@ -1,3 +0,0 @@
-r=int(input("Enter the number of rows:"))
-for i in range(1,r+1):
-    print("*"*i)
