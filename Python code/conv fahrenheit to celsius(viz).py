dc8606d02b4015c05ff5f796ee774fcae732b358
Aspiringtr <127831054+Aspiringtr@users.noreply.github.com>
2023-11-08 19:54:23 +0530

    Delete Python code directory

--- Python code/conv fahrenheit to celsius(viz).py
+++
@@ -1,6 +0,0 @@
-c1=int(input("ENter the value of celsius:"))
-f1=c1*(9/5)+32
-print("Celsius to fahrenheit:",f1)
-f2=int(input("ENter the value of fahrenheit:"))
-c2=((f2-32)*5)/9
-print("fahrenheit to Celsius:",c2)
--- Python code/star pattern.py
+++
@@ -1,4 +0,0 @@
-r=int(input("Enter the number of rown:"))
-for i in range(1,r+1):
-    print("*"*i)
-    
